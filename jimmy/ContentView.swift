eeac3a8dad12f3290c2f724105864675f47a2240
Jonathan Foucher <jfoucher@gmail.com>
2022-02-16 23:10:08 +0100

    Initial Commit

---
+++ jimmy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jimmy
+//
+//  Created by Jonathan Foucher on 16/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ jimmy/jimmyApp.swift
@@ -0,0 +1,17 @@
+//
+//  jimmyApp.swift
+//  jimmy
+//
+//  Created by Jonathan Foucher on 16/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct jimmyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
